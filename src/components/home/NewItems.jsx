5977e0d00c4278aebb398b69b80da4e4c1db6ae6
jralthouse14 <jradrummer95@aol.com>
2026-06-02 14:41:57 -0400

    Final Commit

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -87,7 +87,6 @@ const NewItems = () => {
                     to={`/author/${item.authorId}`}
                     data-bs-toggle="tooltip"
                     data-bs-placement="top"
-                    title="Creator: Monica Lucas"
                   >
                     <img className="lazy" src={item.authorImage} alt="" />
                     <i className="fa fa-check"></i>
@@ -102,15 +101,6 @@ const NewItems = () => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
                       </div>
                     </div>
                   </div>
