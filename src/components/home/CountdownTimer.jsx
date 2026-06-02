1f6de97c1baf1d9b6ff7aca7b1fe182d601501d1
jralthouse14 <jradrummer95@aol.com>
2026-06-02 15:58:06 -0400

    Final Commit

--- src/components/home/CountdownTimer.jsx
+++ src/components/home/CountdownTimer.jsx
@@ -23,7 +23,7 @@ const CountdownTimer = ({ targetDate }) => {
         }, 1000);
 
         return () => clearInterval(timer);
-    }, [calculateTimeLeft]);
+    }, calculateTimeLeft());
 
     return (
         <div>
